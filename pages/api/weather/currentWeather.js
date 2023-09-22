c223b3706b93022e94167ea450700abaebe64948
mick nice <mikemccullagh@outlook.com>
2023-09-22 07:09:11 +0100

    adds various signed in behaviours

--- pages/api/weather/currentWeather.js
+++ pages/api/weather/currentWeather.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getCurrentWeatherReport(coordStr)  {
     return new Promise((resolve, reject) => {
-        axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${coordStr}&aqi=no`)
+        axios.get(`https://api.weatherapi.com/v1/current.json?key=bc6526e02d764054a1061905231809&q=${coordStr}&aqi=no`)
         .then((response) => {
             resolve(response.data)
         })
--- pages/api/weather/forecastDailyWeather.js
+++ pages/api/weather/forecastDailyWeather.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async function getDailyForecastWeatherReport(coordStr)  {
     return new Promise((resolve, reject) => {
       
-    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&q=${coordStr}&aqi=no&days=2&hours=24`)
+    axios.get(`https://api.weatherapi.com/v1/forecast.json?key=bc6526e02d764054a1061905231809&q=${coordStr}&aqi=no&days=2&hours=24`)
         .then((response) => {
             const weatherArr1 = response.data.forecast.forecastday[0].hour
             const weatherArr2 = response.data.forecast.forecastday[1].hour

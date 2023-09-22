4d5135e10f1dbeee0fd1a3fdea409e2bdbe506e2
mick nice <mikemccullagh@outlook.com>
2023-09-22 08:13:45 +0100

    adds various signed in behaviours

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Mike McCullagh - Portfolio</title>
+        <title>The Tardigrade - Refector</title>
         <meta name="description" content="etc. etc."/>
         <link rel="icon" href="/favicon.ico" />
       </Head>

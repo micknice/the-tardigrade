88a4583ec579c066c46af68061fb69205f8b165d
mick nice <mikemccullagh@outlook.com>
2023-09-22 08:17:25 +0100

    adds various signed in behaviours

--- pages/api/weather/currentWeather.js
+++ pages/api/weather/currentWeather.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getCurrentWeatherReport(coordStr) {
   try {
-    const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=bc6526e02d764054a1061905231809&q=${coordStr}&aqi=no`);
+    const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=bc6526e02d764054a1061905231809&q=Leeds&aqi=no`);
     console.log(response.data, 'response.data');
     return response.data;
   } catch (error) {
